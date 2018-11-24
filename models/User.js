22d919f3942f1d54212f98b78f44c2b0a3b44f32
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-24 20:12:34 +0200

    Add comments

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// Schema description.
 var UserSchema = new Schema({
     username:      { type: String, required: true, index: { unique: true }},
     hash:          { type: String, required: true }
 });
 
+// Export model.
 module.exports = mongoose.model('User', UserSchema);

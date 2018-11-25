18165446968976c45d0ca6c8faa8a4514db8abea
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-25 05:43:02 +0200

    Add minlength and maxlength to model, add placeholders for password hashing and authentication

--- models/User.js
+++ models/User.js
@@ -3,8 +3,18 @@ var Schema = mongoose.Schema;
 
 // Schema description.
 var UserSchema = new Schema({
-    username:      { type: String, required: true, index: { unique: true }},
-    hash:          { type: String, required: true }
+    username:      { type: String, required: true, index: { unique: true }, minlength: 4, maxlength: 100 },
+    password:      { type: String, required: true, minlength: 10, maxlength: 100 }
+});
+
+// Prehook for hashing passwords before saving.
+UserSchema.pre('save', (next) => {
+    // do hashing here
+});
+
+// Static method for authenticating user.
+UserSchema.static('authenticate', (username, password) => {
+    // authenticate here
 });
 
 // Export model.

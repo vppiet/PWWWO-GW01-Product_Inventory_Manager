4cfd91399c1726514ee7eb83a0d1c2fdb537c08a
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-27 03:42:32 +0200

    Add routes for overview, products and, creating product; add controller functions for overview, listing products, and product form

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,62 @@
 var Product = require('../models/product');
+var ProductCategory = require('../models/productcategory');
+var Producer = require('../models/producer');
+var Vendor = require('../models/vendor');
+var async = require('async');
 
-// Display list of all products.
-module.exports.overview = ((req, res, next) => {
-    Product.find({}, ('name product_type in_stock'))
-        .populate('product_type')
-        .exec((err, products) => {
-            if (err) { return (next(err)); }
-            res.render('product_list', { title: 'Overview', product_list: products});
-        });
-});
+// Overview: Display counts of collections.
+module.exports.overview = (req, res, next) => {
+
+    async.parallel({
+        product_count: (callback) => {
+            Product.countDocuments(callback);
+        },
+        productcategory_count: (callback) => {
+            ProductCategory.countDocuments(callback);
+        },
+        producer_count: (callback) => {
+            Producer.countDocuments(callback);
+        },
+        vendor_count: (callback) => {
+            Vendor.countDocuments(callback);
+        }
+    }, (err, results) => {
+        if (err) { return next(err); }
+        res.render('overview', { title: 'Overview', data: results });
+    });
+
+};
+
+// List products.
+module.exports.product_list = (req, res, next) => {
+
+    Product.find({})
+    .populate('category')
+    .populate('vendor')
+    .populate('producer')
+    .exec((err, results) => {
+        if (err) { return next(err); }
+        res.render('product_list', { title: 'Products', products: results });
+    });
+
+};
+
+// Create product.
+module.exports.product_create_get = (req, res, next) => {
+
+    async.parallel({
+        categories: (callback) => {
+            ProductCategory.find(callback)
+        },
+        vendors: (callback) => {
+            Vendor.find(callback)
+        },
+        producers: (callback) => {
+            Producer.find(callback)
+        }
+    }, (err, results) => {
+        if (err) { return next(err); }
+        res.render('product_form', { title: 'Create Product', categories: results.categories, vendors: results.vendors, producers: results.producers });
+    });
+
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -11,6 +11,11 @@ router.get('/', product_controller.overview);
 
 /// PRODUCT ROUTES ///
 
+// GET for listing products.
+router.get('/products', product_controller.product_list);
+
+// GET for creating product.
+router.get('/products/create', product_controller.product_create_get);
 
 /// VENDOR ROUTES ///
 

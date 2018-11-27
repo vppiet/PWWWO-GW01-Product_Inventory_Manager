ca6b94d47ad5eb29839d4f6ed82837cb6f289021
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-11-27 08:37:08 +0200

    Add product routes (commented are not implemented)

--- routes/inventory.js
+++ routes/inventory.js
@@ -5,19 +5,34 @@ var router = express.Router();
 var product_controller = require('../controllers/productController');
 
 /// INDEX ///
-
-// GET inventory home page (overview).
+// GET for inventory home page (overview).
 router.get('/', product_controller.overview);
 
 /// PRODUCT ROUTES ///
+// GET:     create a product.
+router.get('/product/create', product_controller.product_create_get);
+// POST:    create a product.
+//router.post('/product/create', product_controller.product_create_post);
+//// GET:     delete a product
+//router.get('/product/:id/delete', product_controller.product_delete_get);
+//// POST:    delete a product
+//router.get('/product/:id/', product_controller.product_delete_post);
+//// GET:     update a product
+//router.get('/product/:id/update', product_controller.product_update_get);
+//// GET:     update a product
+//router.get('/product/:id/update', product_controller.product_update_post);
+//// GET:     product details
+router.get('/product/:id', product_controller.product_detail);
+// GET:     list products.
+router.get('/product', product_controller.product_list);
 
-// GET for listing products.
-router.get('/products', product_controller.product_list);
+/// CATEGORY ROUTES ///
 
-// GET for creating product.
-router.get('/products/create', product_controller.product_create_get);
 
 /// VENDOR ROUTES ///
+
+
+/// PRODUCER ROUTES ///
 
 
 module.exports = router;

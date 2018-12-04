57020c5f29e1198a339bfd91b699bb6e435ea4f2
Ville Pietarinen <ville.pietarinen@edu.turkuamk.fi>
2018-12-04 11:54:42 +0200

    Add virtual property for product category URL

--- models/productcategory.js
+++ models/productcategory.js
@@ -2,10 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // Schema description.
-var ProductTypeSchema = new Schema({
+var ProductCategorySchema = new Schema({
     name:           { type: String, required: true, minlength: 1, maxlength: 100 },
     description:    { type: String, minlength: 1, maxlength: 1000 }
 });
 
+// Virtual for product category URL.
+ProductCategorySchema
+.virtual('url')
+.get(function () {
+    return '/inventory/productcategory/' + this._id;
+});
+
 // Export model.
-module.exports = mongoose.model('ProductCategory', ProductTypeSchema);
+module.exports = mongoose.model('ProductCategory', ProductCategorySchema);
